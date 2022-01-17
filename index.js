ff5f749865a1615d332354defc6ea4adf81a5288
Michael Gutierrez <michaelgutierrezj@gmail.com>
2022-01-17 13:30:58 -0600

    task 4 commit

--- index.js
+++ index.js
@@ -28,11 +28,17 @@ console.log(processFirstItem(['foo','bar'],function(str){return str+str}));
   Study the code for counter1 and counter2, then answer the questions below.
   
   1. What is the difference between counter1 and counter2?
+    -counter1 declares the count variable privately inside the countermaker function
+    while counter2 declares count in global space
   
   2. Which of the two uses a closure? How can you tell?
+    -counter1 uses closure, the anonymous function inside function counterMaker uses closure to access the 
+    variable counter andvanceing it by one and returning 
   
   3. In what scenario would the counter1 code be preferable? In what scenario would 
      counter2 be better?  
+     - if the variable count is not needed elsewhere counter 1 keeps count private, but if count is needed by
+     other code counter keeps it abaiable
 */
 
 // counter1 code
@@ -63,7 +69,7 @@ NOTE: This will be a callback function for the tasks below
 */
 
 function inning(/*Code Here*/){
-    /*Code Here*/
+    return Math.floor(Math.random() * 3);
 }
 
 
@@ -81,8 +87,14 @@ Use the finalScore function below to do the following:
 }
 */ 
 
-function finalScore(/*code Here*/){
-  /*Code Here*/
+function finalScore(inningCB, innings){
+  let away = 0;
+  let home = 0
+  for(let i = 0; i < innings; i++){
+    away += inningCB()
+    home += inningCB()
+  }
+  return {Away: away, Home: home};
 }
 
 /* ⚾️⚾️⚾️ Task 4: getInningScore() ⚾️⚾️⚾️
@@ -90,8 +102,8 @@ Use the getInningScore() function below to do the following:
   1. Receive a callback function - you will pass in the inning function from task 2 as your argument 
   2. Return an object with a score for home and a score for away that populates from invoking the inning callback function */
 
-function getInningScore(/*Your Code Here */) {
-  /*Your Code Here */
+function getInningScore(inningCB) {
+  return {Away:  inningCB(), Home:  inningCB()}
 }
 
 
